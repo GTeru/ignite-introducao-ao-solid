01ae5eeee7bc9ce06950fbc416f17ecc1d9e237d
GTeru <gmteruya@hotmail.com>
2022-04-15 10:09:48 -0300

    finalizado desafio documentação

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
 import { app } from ".";
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.config.json';
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
